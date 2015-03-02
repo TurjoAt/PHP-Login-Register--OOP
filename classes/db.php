751d99cba41cc4ee61b38ccb92899a8c41520473
RajeebTheGreat <rajeebthegreat@gmail.com>
2015-03-03 02:16:48 +0545

    [DB Results] Fetching data with methods

--- classes/db.php
+++ classes/db.php
@@ -97,6 +97,16 @@ class DB{
 		return $this->action('DELETE', $table, $where);
 	}
 
+	public function results()
+	{
+		return $this->_results;
+	}
+
+	public function first()
+	{
+		return $this->results()[0];
+	}
+
 	public function error()
 	{
 		return $this->_error;
