6733e38527474c558735770487ecfb71a93645ad
RajeebTheGreat <rajeebthegreat@gmail.com>
2015-03-04 08:54:37 +0545

    [Form] Basic Register Form

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+</head>
+<body>
+	<form action="" method="POST">
+		<div class="field">
+			<label for="username">Username:</label>
+			<input type="text" name="username" id="username" value="" autocomplete="off">
+		</div>
+
+		<div class="field">
+		<label for="password">Password:</label>
+			<input type="password" name="password" id="password">
+		</div>
+
+		<div class="field">
+		<label for="password_again">Repeat Password:</label>
+			<input type="password" name="password_again" id="password_again">
+		</div>
+
+		<div class="field">
+		<label for="name">Name:</label>
+			<input type="text" name="name" id="name">
+		</div>
+
+		<input type="submit" value="Register me">
+	</form>
+</body>
+</html>
