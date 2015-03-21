10bfb58554221d17786943a903113af179ea88d2
RajeebTheGreat <rajeebthegreat@gmail.com>
2015-03-21 15:30:41 +0545

    Checking signed in user using session

--- index.php
+++ index.php
@@ -6,3 +6,17 @@ if(Session::exists('home'))
 {
 	echo '<p>' .Session::flash('home').'</p>';
 }
+
+$user = new User();
+
+if($user->isLoggedIn()) {
+?>
+	<p>Hello, <a href="#"><?php echo escape($user->data()->username); ?></a>!
+
+	<ul>
+		<li><a href="logout.php">Log Out</a></li>
+	</ul>
+<?php
+} else {
+	echo '<p>You need to <a href="login.php">login</a> or <a href="register.php">register</a>';
+}
--- login.php
+++ login.php
@@ -22,7 +22,7 @@
 				$login 	= $user->login(Input::get('username'), Input::get('password'));
 				if($login)
 				{
-					echo "Success";
+					Redirect::to('index.php');
 				}
 				else{
 					echo "sorry! Failed";
